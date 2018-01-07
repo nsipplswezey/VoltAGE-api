4bf6183ee46ec10c8249e10e2651140764c629d1
Nick S <nsipplswezey@gmail.com>
2018-01-06 23:05:04 -0800

    Adds new predictor

--- tasks/update_seed.js
+++ tasks/update_seed.js
@@ -8,32 +8,36 @@ class UpdateSeedTask {
 
     console.log('Executing update seed');
 
-	let paths = ['/../VoltAGE_1_predictor.txt','/../VoltAGE_2_predictor.txt']
+    //Get file names
+	let fileNames = fs.readdirSync(__dirname + '/../predictors')
+    console.log('Adding ' + fileNames + " to seed.")
 
-	paths.forEach((path,index,collection) => {
+    //Read each file, read the seed, create predictor seed data object, add to seed
+	fileNames.forEach((name,index,collection) => {
 
-      let model = fs.readFileSync(__dirname + path, 'utf8')
+      //Read each file
+	  let path = __dirname + '/../predictors/' + name 
+      let model = fs.readFileSync(path, 'utf8')
 
-      let seed = fs.readFileSync(__dirname + '/../config/seed.json', 'utf8')
-
+      //Read the seed.json and parse it into an object 
+	  let seed = fs.readFileSync(__dirname + '/../config/seed.json', 'utf8')
       let seedObject = JSON.parse(seed)
 
+      //Create a predictor seed data object
 	  let predictor = {}
 	  predictor.history = {} 
 	  predictor.parameters = model 
       
+	  //Add it to the seed
 	  seedObject.development.Detector.push(predictor)
       seedObject.production.Detector.push(predictor)
 
-	  try {
-	    let seedString = JSON.stringify(seedObject,null,2)
-		console.log(seedString)
-		console.log(seedObject)
-        fs.writeFileSync(__dirname + '/../config/seed.json', seedString, 'utf8') 
-        console.log('Seed updated');
-	  } catch(e) {
-	    console.error(e)
-	  }
+      //Stringify and write the seed
+	  let seedString = JSON.stringify(seedObject,null,2)
+      console.log('Current seed object shape')
+      console.log(seedObject)
+      fs.writeFileSync(__dirname + '/../config/seed.json', seedString, 'utf8') 
+      console.log('Seed updated');
 		
 	})
     //callback();
